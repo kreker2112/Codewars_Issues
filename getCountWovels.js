687fbc6f473a8e8b8be4ba1f40155235adb9be22
kreker2112 <kreker2112@gmail.com>
2023-05-30 18:46:41 +0300

    Codewars update

---
+++ getCountWovels.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str) {
+    let wovelsCount = 0;
+    const wovels = ['a', 'e', 'i', 'o', 'u'];
+    for (let char of str){
+        if (wovels.includes(char)){
+            wovelsCount +=1  
+        }
+    }
+    return wovelsCount;
+  }
+
+  console.log(getCount("abracadabra"));
