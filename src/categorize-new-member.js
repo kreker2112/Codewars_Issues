73aa74a6c59feb5b1a638ed3ab2e60dc879cb7e6
kreker2112 <kreker2112@gmail.com>
2023-06-13 14:58:15 +0300

    codewars update

--- src/categorize-new-member.js
+++ src/categorize-new-member.js
@@ -1,9 +1,9 @@
-'use strict'; 
+"use strict";
 
-// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. 
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open.
 // They would like your help with an application form that will tell prospective members which category they will be placed.
 
-// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. 
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7.
 // In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
 
 // Input
@@ -16,6 +16,44 @@
 // input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
-function openOrSenior(data){
-    let result;
+function openOrSenior(data) {
+  let resultArray = [];
+  for (let i = 0; i < data.length; i += 1) {
+    console.log(data[i][1]);
+    data[i][0] >= 55 && data[i][1] > 7
+      ? resultArray.push("Senior")
+      : resultArray.push("Open");
   }
+  return resultArray;
+}
+
+// Вариант 2
+
+// function openOrSenior(data){
+//   return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+// }
+
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ])
+);
+console.log(
+  openOrSenior([
+    [3, 12],
+    [55, 1],
+    [91, -2],
+    [53, 23],
+  ])
+);
+console.log(
+  openOrSenior([
+    [59, 12],
+    [55, -1],
+    [12, -2],
+    [12, 12],
+  ])
+);
